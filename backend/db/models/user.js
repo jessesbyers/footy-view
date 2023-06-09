55c9fee26d072bae329e8f1be57e4b400796b5da
Jesse Byers <jesse.s.byers@gmail.com>
2023-06-09 10:48:26 -0400

    added backend auth

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -7,52 +7,51 @@ module.exports = (sequelize, DataTypes) => {
         // define association here
         }
     }
-    User.init(
-        {
-            username: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    len: [4, 30],
-                    isNotEmail(value) {
-                        if (Validator.isEmail(value)) {
-                            throw new Error("Cannot be an email.");
-                        }
+    User.init({
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4, 30],
+                isNotEmail(value) {
+                    if (Validator.isEmail(value)) {
+                        throw new Error("Cannot be an email.");
                     }
                 }
-            },
-            firstName: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            lastName: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            email: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    len: [3, 256],
-                    isEmail: true
-                }
-            },
-            hashedPassword: {
-                type: DataTypes.STRING.BINARY,
-                allowNull: false,
-                validate: {
-                    len: [60, 60]
-                }
             }
-        }, {
-            sequelize,
-            modelName: 'User',
-            defaultScope: {
-                attributes: {
-                  exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
-                }
-              }
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [3, 256],
+                isEmail: true
+            }
+        },
+        hashedPassword: {
+            type: DataTypes.STRING.BINARY,
+            allowNull: false,
+            validate: {
+                len: [60, 60]
+            }
         }
-    );
+    }, 
+    { 
+        sequelize,
+        modelName: 'User',
+        defaultScope: {
+            attributes: {
+                exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+            }
+        }
+    });
     return User;
 };
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 router.use(restoreUser);
 
@@ -7,12 +9,14 @@ const { User } = require('../../db/models');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
 // TEST ROUTE
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-// });
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
 
 // TEST ROUTE: GET /api/set-token-cookie
 // router.get('/set-token-cookie', async (_req, res) => {
