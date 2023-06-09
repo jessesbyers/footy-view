dbcf8d66a8dc9b04f3132a72dc94e9e540d93f3e
Jesse Byers <jesse.s.byers@gmail.com>
2023-06-09 13:39:20 -0400

    added frontend auth and basic nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import * as sessionActions from "./store/session";
+
+import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
+import Navigation from "./components/Navigation";
+
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
-  );
+    const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+    }, [dispatch]);
+  
+    return (
+        <>
+            <Navigation isLoaded={isLoaded} />
+
+            {isLoaded && (
+                <Switch>
+                <Route path="/login">
+                    <LoginFormPage />
+                </Route>
+                <Route path="/signup">
+                    <SignupFormPage />
+                </Route>
+                </Switch>
+            )}
+        </>
+    );
 }
+
 
 export default App;
